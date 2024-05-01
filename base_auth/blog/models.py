6bddc732844c894d86b0203413477f91cd6d48db
newxxson <hyukjun1111@gmail.com>
2024-05-01 21:17:29 +0900

    session14용 s3 사용 코드 구현

--- base_auth/blog/models.py
+++ base_auth/blog/models.py
@@ -6,6 +6,7 @@ from django.conf import settings
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+    image_url = models.URLField(blank=True, null=True)
     creator = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
     )
--- base_auth/blog/utils.py
+++ base_auth/blog/utils.py
@@ -1,5 +1,7 @@
 import datetime
 from functools import wraps
+import re
+import unicodedata
 from pytz import timezone
 from django.shortcuts import get_object_or_404, render
 from .models import Post
@@ -31,3 +33,31 @@ def update_post_lasted_viewed():
         return _wrapped_view
 
     return decorator
+
+
+def get_new_file_name(file, user, title):
+    file_extension = file.name.split(".")[-1]
+    cleaned_title = clean_title(title)
+    new_file_name = (
+        cleaned_title
+        if cleaned_title
+        else datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    )
+    new_file_name = f"blog/{user.username}/{new_file_name}.{file_extension}"
+    return new_file_name
+
+
+def clean_title(title):
+    """
+    Clean and sanitize the title by removing or replacing special characters.
+    """
+    title = (
+        unicodedata.normalize("NFKD", title).encode("ascii", "ignore").decode("ascii")
+    )  # Normalize and remove non-ascii chars
+    title = re.sub(
+        r"[^\w\s-]", "", title
+    ).strip()  # Remove special characters except for underscores, hyphens, and whitespace
+    title = re.sub(
+        r"[-\s]+", "-", title
+    )  # Replace whitespace or sequence of dashes with a single dash
+    return title
